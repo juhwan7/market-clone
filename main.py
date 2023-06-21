38ed73f1bcb63080fe726e3c1acfb83ae586c4be
김주환 <kjh71047@naver.com>
2023-06-21 17:51:28 +0900

    당근마켓 95%완성
    로그인시 401오류 원인 못찾음

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI, UploadFile, Form, Response
+from fastapi import FastAPI, UploadFile, Form, Response, Depends
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
+from fastapi_login import LoginManager
+from fastapi_login.exceptions import InvalidCredentialsException
 from typing import Annotated
 import sqlite3
 
@@ -24,8 +26,61 @@ cur.execute(f"""
 app = FastAPI()
 
 
+SERCRET = "super-coding"
+manager = LoginManager(SERCRET, '/login')
+
+
+@manager.user_loader()
+def query_user(data):
+    WHERE_STATEMENTS = f'id="{data}"'
+    if type(data) == dict:
+        WHERE_STATEMENTS = f'name="{id["id"]}"'
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    user = cur.execute(f"""
+                       SELECT * FROM users WHERE {WHERE_STATEMENTS}
+                       """).fetchone()
+    return user
+
+
+@app.post('/login')
+def login(
+        id: Annotated[str, Form()],
+        password: Annotated[str, Form()]):
+    user = query_user(id)
+    print(user['password'])
+    if not user:
+        raise InvalidCredentialsException
+    elif password != user['password']:
+        raise InvalidCredentialsException
+
+    access_token = manager.create_access_token(data={
+        'sub': {
+            'id': user['id'],
+            'name': user['name'],
+            'email': user['email'],
+        }
+    })
+
+    return {'access_token': access_token}
+
+
+@app.post('/signup')
+def signup(id: Annotated[str, Form()],
+           password: Annotated[str, Form()],
+           name: Annotated[str, Form()],
+           email: Annotated[str, Form()],
+           ):
+    cur.execute(f"""
+                INSERT INTO users(id,name,email,password)
+                VALUES('{id}','{name}','{email}','{password}')
+                """)
+    con.commit()
+    return "200"
+
+
 @app.get("/items")
-async def get_items():
+async def get_items(user=Depends(manager)):
     # 컬럼명도 같이 가져옴
     con.row_factory = sqlite3.Row
     cur = con.cursor()
@@ -61,20 +116,6 @@ async def get_image(item_id):
                               WHERE id={item_id}
                               """).fetchone[0]
     return Response(content=bytes.fromhex(image_bytes), media_type="image/*")
-
-
-@app.post('/signup')
-def signup(id: Annotated[str, Form()],
-           password: Annotated[str, Form()],
-           name: Annotated[str, Form()],
-           email: Annotated[str, Form()],
-           ):
-    cur.execute(f"""
-                INSERT INTO users(id,name,email,password)
-                VALUES('{id}','{name}','{email}','{password}')
-                """)
-    con.commit()
-    return "200"
 
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
