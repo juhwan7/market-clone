7f48776481900c776eb8a38ff7e993ae8c8c6129
김주환 <kjh71047@naver.com>
2023-07-08 14:11:02 +0900

    유저 인증까지 파이어베이스로 구현

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,48 @@
   import NotFound from "./pages/NotFound.svelte";
   import Signup from "./pages/Signup.svelte";
   import Write from "./pages/Write.svelte";
-  import Login from "./pages/login.svelte";
+  import Login from "./pages/Login.svelte";
+  import MyPage from "./pages/MyPage.svelte";
   import Router from "svelte-spa-router";
   import "./css/style.css";
+  import { user$ } from "./store";
+  import {
+    GoogleAuthProvider,
+    getAuth,
+    signInWithCredential,
+  } from "firebase/auth";
+  import { onMount, tick } from "svelte";
+  import Loading from "./pages/Loading.svelte";
+
+  let isLoading = true;
+
+  const auth = getAuth();
+
+  const checkLogin = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return (isLoading = false);
+    const credential = GoogleAuthProvider.credential(null, token);
+    const result = await signInWithCredential(auth, credential);
+    const user = result.user;
+    user$.set(user);
+    isLoading = false;
+  };
 
   const routes = {
     "/": Main,
-    "/login": Login,
     "/signup": Signup,
     "/write": Write,
+    "/my": MyPage,
     "*": NotFound,
   };
+
+  onMount(() => checkLogin());
 </script>
 
-<Router {routes} />
+{#if isLoading}
+  <Loading />
+{:else if !$user$}
+  <Login />
+{:else}
+  <Router {routes} />
+{/if}
---
+++ src/pages/Loading.svelte
@@ -0,0 +1,7 @@
+<div>로딩중입니다</div>
+
+<style>
+  div {
+    font-size: 300px;
+  }
+</style>
